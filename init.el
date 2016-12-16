0323d9a0317075c917bbdc79d1f9fb8197beb647
Noah Morton <noahmorton98@gmail.com>
2016-12-15 22:23:31 -0600

    Update config

--- init.el
+++ init.el
@@ -8,6 +8,8 @@
  ;; custom-set-faces was added by Custom.
  '(default ((t (:family "Terminus (TTF)" :foundry "unknown" :slant normal :weight normal :height 113 :width normal)))))
 
+;; Begin normal setup
+
 ;; General settings ------------------
 
 ;; Change backup directory to system temp directory
@@ -15,21 +17,30 @@
           `((".*" . ,temporary-file-directory)))
     (setq auto-save-file-name-transforms
           `((".*" ,temporary-file-directory t)))
-;; Change default abbreviations file
- (setq abbrev-file-name "~/.emacs.d/abbrev_defs")  
-;; Enable abbrev-mode by default
-(setq-default abbrev-mode t)
 
 
+ (setq abbrev-file-name "~/.emacs.d/abbrev_defs") ;; Change default abbreviations file
+(setq-default abbrev-mode t) ;; Enable abbrev-mode by default
+(global-hl-line-mode) ;; Highlight current cursor line
+(scroll-bar-mode -1) ;; Disable the scroll bar
+(prefer-coding-system 'utf-8) ;; Prefer UTF-8 encoding
 
 
 ;; Hooks ------------------------
 ;; Text mode hooks
 (add-hook 'text-mode-hook 'flyspell-mode) ;; Turn on incorrect spell highlight
 
+;;Elisp mode hooks
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 
 
 ;; Keybindings ---------------------------------
 
 ;; Use Ctrl-C e to open the init file for changing config.
 (global-set-key (kbd "C-c e") (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
+
+;; Major mode definitions -------------------------
+(define-derived-mode mediawiki-mode text-mode "Mediawiki"
+  "Major mode for mediawiki articles. \\{mediawiki-mode-map}"
+  'abbrev-table "mediawiki-mode-abbrev-table")
+
