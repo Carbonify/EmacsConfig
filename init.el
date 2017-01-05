73768af5287f702c01a01eeb7496a7cce622b428
Noah Morton <noahmorton98@gmail.com>
2017-01-04 22:18:44 -0600

    Update config

--- init.el
+++ init.el
@@ -12,10 +12,10 @@
  '(default ((t (:family "Terminus (TTF)" :foundry "unknown" :slant normal :weight normal :height 113 :width normal)))))
 
 ;; Change backup directory to system temp directory
- (setq backup-directory-alist
-          `((".*" . ,temporary-file-directory)))
-    (setq auto-save-file-name-transforms
-          `((".*" ,temporary-file-directory t)))
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
 
 
 (setq abbrev-file-name "~/.emacs.d/abbrev_defs") ;; Change default abbreviations file
@@ -23,6 +23,10 @@
 (global-hl-line-mode) ;; Highlight current cursor line
 (scroll-bar-mode -1) ;; Disable the scroll bar
 (prefer-coding-system 'utf-8) ;; Prefer UTF-8 encoding
+(add-to-list 'load-path "~/.emacs.d/manual-install/") ;; Add dir for manually installed plugins
+(electric-indent-mode 1) ;; Always make newline keep indent
+(electric-pair-mode 1) ;; Pair parens and other brackets
+
 
 ;; Mode creation ------------------------------
 
@@ -31,14 +35,14 @@
    \\{mediawiki-mode-map}"
   :abbrev-table nil
   (defun create-mediawiki-table () "Creates a mediawiki table."
-      (interactive)
-      (insert "{| class=\"wikitable\"") (newline)
-      (insert "|-") (newline)
-      (insert "!Option1!!Option2!!Option3") (newline)
-      (insert "|-") (newline)
-      (insert "|Value1 || Value2 || Value3") (newline)
-      (insert "|-") (newline) 
-      (insert "|}"))
+    (interactive)
+    (insert "{| class=\"wikitable\"") (newline)
+    (insert "|-") (newline)
+    (insert "!Option1!!Option2!!Option3") (newline)
+    (insert "|-") (newline)
+    (insert "|Value1 || Value2 || Value3") (newline)
+    (insert "|-") (newline) 
+    (insert "|}"))
 
   (defun create-mediawiki-collapse () "Creates the HTML source code for a collapsible text field."
     (interactive)
@@ -48,7 +52,6 @@
     (insert "Hidden Content") (newline)
     (insert "</div></div>") (newline))
 
-
   (define-key mediawiki-mode-map (kbd "C-c C-w t") 'create-mediawiki-table)
   (define-key mediawiki-mode-map (kbd "C-c C-w c") 'create-mediawiki-collapse))
 
@@ -57,6 +60,8 @@
   "Major mode for markdown documents.
 \\{markdown-mode-map}"
   :abbrev-table nil)
+
+;; Plugin config -----------------
 
 
 ;; Hooks ------------------------
@@ -71,6 +76,25 @@
 
 ;; Use Ctrl-C e to open the init file for changing config.
 (global-set-key (kbd "C-c e") (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
+
+;; Tab complete
+(global-set-key [(tab)] 'smart-tab)
+(defun smart-tab ()
+  "This smart tab is minibuffer compliant: it acts as usual in
+    the minibuffer. Else, if mark is active, indents region. Else if
+    point is at the end of a symbol, expands it. Else indents the
+    current line."
+  (interactive)
+  (if (minibufferp)
+      (unless (minibuffer-complete)
+        (dabbrev-expand nil))
+    (if mark-active
+        (indent-region (region-beginning)
+                       (region-end))
+      (if (looking-at "\\_>")
+          (dabbrev-expand nil)
+        (indent-for-tab-command)))))
+
 
 ;; Filetype detection --------------------
 
