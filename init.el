478034efde05650fff8b57c4989c2d07e31d90f6
Noah Morton <noahmorton98@gmail.com>
2017-06-11 09:23:33 -0500

    Changed some spaces to tabs, experimenting

--- init.el
+++ init.el
@@ -104,22 +104,27 @@
 (require 'windmove)
 (windmove-default-keybindings)
 
+(eval-after-load 'lua-mode
+  (progn
+	(setq lua-indent-level 4)
+	))
+
 ;; Eshell
 (setq eshell-prompt-function
-      (lambda ()
-        (concat
-         (propertize "┌─[" 'face `(:foreground "green"))
-         (propertize (user-login-name) 'face `(:foreground "red"))
-         (propertize "@" 'face `(:foreground "green"))
-         (propertize (system-name) 'face `(:foreground "blue"))
-         (propertize "]──[" 'face `(:foreground "green"))
-         (propertize (format-time-string "%H:%M" (current-time)) 'face `(:foreground "yellow"))
-         (propertize "]──[" 'face `(:foreground "green"))
-         (propertize (concat (eshell/pwd)) 'face `(:foreground "white"))
-         (propertize "]\n" 'face `(:foreground "green"))
-         (propertize "└─>" 'face `(:foreground "green"))
-         (propertize (if (= (user-uid) 0) " # " " $ ") 'face `(:foreground "green"))
-         )))
+	  (lambda ()
+		(concat
+		 (propertize "┌─[" 'face `(:foreground "green"))
+		 (propertize (user-login-name) 'face `(:foreground "red"))
+		 (propertize "@" 'face `(:foreground "green"))
+		 (propertize (system-name) 'face `(:foreground "blue"))
+		 (propertize "]──[" 'face `(:foreground "green"))
+		 (propertize (format-time-string "%H:%M" (current-time)) 'face `(:foreground "yellow"))
+		 (propertize "]──[" 'face `(:foreground "green"))
+		 (propertize (concat (eshell/pwd)) 'face `(:foreground "white"))
+		 (propertize "]\n" 'face `(:foreground "green"))
+		 (propertize "└─>" 'face `(:foreground "green"))
+		 (propertize (if (= (user-uid) 0) " # " " $ ") 'face `(:foreground "green"))
+		 )))
 
 ;; Hooks ------------------------
 
@@ -141,15 +146,15 @@
 ;; Org mode hooks
 (eval-after-load 'org
   (progn
-    (defun user--org-mode-config () "Configuration for org mode."
-      (interactive)
-      (define-key org-mode-map [tab] 'hippie-expand) ;; Set these two, because I use tab complete much more than cycle
-      (define-key org-mode-map (kbd "C-<home>") 'org-global-cycle)
-      (setq org-log-done t)
-      (setq fill-column 100))
-    (add-hook 'org-mode-hook 'user--org-mode-config)
-    (add-hook 'org-mode-hook 'visual-line-mode) ;; Make org just wrap long lines
-    (add-hook 'org-mode-hook 'auto-fill-mode)))
+	(defun user--org-mode-config () "Configuration for org mode."
+	  (interactive)
+	  (define-key org-mode-map [tab] 'hippie-expand) ;; Set these two, because I use tab complete much more than cycle
+	  (define-key org-mode-map (kbd "C-<home>") 'org-global-cycle)
+	  (setq org-log-done t)
+	  (setq fill-column 100))
+	(add-hook 'org-mode-hook 'user--org-mode-config)
+	(add-hook 'org-mode-hook 'visual-line-mode) ;; Make org just wrap long lines
+	(add-hook 'org-mode-hook 'auto-fill-mode)))
 
 ;; Programming mode hooks
 (defun user--prog-hippie-expand-setting () (interactive)
@@ -169,11 +174,11 @@
 (global-set-key (kbd "C-<tab>") 'indent-for-tab-command) ;; Old function of tab
 
 ;; Make using ISearch much easier
-(define-key isearch-mode-map [next]     'isearch-repeat-forward)
-(define-key isearch-mode-map [prior]    'isearch-repeat-backward)
-(global-set-key (kbd "C-s")             'isearch-forward-regexp)
-(global-set-key (kbd "C-r")             'isearch-backward-regexp)
-(global-set-key (kbd "C-M-s")           'multi-isearch-buffers-regexp)
+(define-key isearch-mode-map [next]		'isearch-repeat-forward)
+(define-key isearch-mode-map [prior]	'isearch-repeat-backward)
+(global-set-key (kbd "C-s")				'isearch-forward-regexp)
+(global-set-key (kbd "C-r")				'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s")			'multi-isearch-buffers-regexp)
 (global-unset-key (kbd "C-M-r"))
 
 ;; Search all loaded buffers for a regex
@@ -183,21 +188,21 @@
 (global-set-key [f7] 'user--search-all-buffers)
 
 ;; Unbind arrow keys, use them for specialized movement instead
-(global-set-key (kbd "<right>")    'user--mark-ring-forward)
+(global-set-key (kbd "<right>")	   'user--mark-ring-forward)
 (global-set-key (kbd "C-<right>")  'next-buffer)
-(global-set-key (kbd "<up>")       'scroll-down-command)
+(global-set-key (kbd "<up>")	   'scroll-down-command)
 (global-set-key (kbd "C-<left>")   'previous-buffer)
-(global-set-key (kbd "<left>")     'pop-to-mark-command)
-(global-set-key (kbd "<down>")     'scroll-up-command)
+(global-set-key (kbd "<left>")	   'pop-to-mark-command)
+(global-set-key (kbd "<down>")	   'scroll-up-command)
 
 ;; Bind home and end to use point register commands
 (defun user--safe-point-to-register (register)
   "Asks for confirmation before overwriting an existing register with a point-to-register."
   (interactive "cRegister:")
   (if (not (get-register register))
-      (point-to-register register)
-    (if (y-or-n-p "Replace existing register?")
-        (point-to-register register))))
+	  (point-to-register register)
+	(if (y-or-n-p "Replace existing register?")
+		(point-to-register register))))
 
 (global-set-key (kbd "<home>") 'jump-to-register)
 (global-set-key (kbd "<end>") 'user--safe-point-to-register)
@@ -205,10 +210,10 @@
 
 ;; Disable moving point with clicks
 (dolist (k '([mouse-1] [down-mouse-1] [drag-mouse-1] [double-mouse-1] [triple-mouse-1]
-             [mouse-2] [down-mouse-2] [drag-mouse-2] [double-mouse-2] [triple-mouse-2]
-             [mouse-3] [down-mouse-3] [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
-             [mouse-4] [down-mouse-4] [drag-mouse-4] [double-mouse-4] [triple-mouse-4]
-             [mouse-5] [down-mouse-5] [drag-mouse-5] [double-mouse-5] [triple-mouse-5]))
+			 [mouse-2] [down-mouse-2] [drag-mouse-2] [double-mouse-2] [triple-mouse-2]
+			 [mouse-3] [down-mouse-3] [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
+			 [mouse-4] [down-mouse-4] [drag-mouse-4] [double-mouse-4] [triple-mouse-4]
+			 [mouse-5] [down-mouse-5] [drag-mouse-5] [double-mouse-5] [triple-mouse-5]))
   (global-unset-key k))
 
 
@@ -230,19 +235,19 @@
 (global-set-key (kbd "C-<return>") 'user--end-of-line-newline)
 
 ;; External function binds, emacs binds
-(global-set-key (kbd "C-x C-b")    'ibuffer) ;; Interactive buffer switch
-(global-set-key (kbd "C-c s")      'eshell) ;; Open a shell easily in emacs
+(global-set-key (kbd "C-x C-b")	   'ibuffer) ;; Interactive buffer switch
+(global-set-key (kbd "C-c s")	   'eshell) ;; Open a shell easily in emacs
 (global-set-key (kbd "<mouse-9>")  'beginning-of-buffer) ;;Move to the beginning and end of buffer with mouse buttons.
 (global-set-key (kbd "<mouse-8>")  'end-of-buffer)
-(global-set-key (kbd "C-c d p")    'user--delete-in-parentheses) ;; Delete text within parentheses.
-(global-set-key (kbd "C-c d q")    'user--delete-in-quotes) ;; Delete text within quotes.
-(global-set-key (kbd "C-c d b")    'user--delete-in-brackets) ;; Delete text within brackets, eg [],{}, <>
-(global-set-key (kbd "C-x C-d")    'user--insert-date) ;; Insert the date
-(global-set-key (kbd "<f8>")       'neotree-toggle)
-(global-set-key (kbd "C-c a")      'align-regexp)
-(global-set-key (kbd "C-z")        'repeat) ;;stop accidentally hitting this and minimizing
-(global-set-key (kbd "C-c f")      'follow-delete-other-windows-and-split) ;; Enter follow mode quickly
-(global-set-key (kbd "C-c n")      'user--make-temp-file) ;;Create a temporary file that's a bit more persistant than scratch
+(global-set-key (kbd "C-c d p")	   'user--delete-in-parentheses) ;; Delete text within parentheses.
+(global-set-key (kbd "C-c d q")	   'user--delete-in-quotes) ;; Delete text within quotes.
+(global-set-key (kbd "C-c d b")	   'user--delete-in-brackets) ;; Delete text within brackets, eg [],{}, <>
+(global-set-key (kbd "C-x C-d")	   'user--insert-date) ;; Insert the date
+(global-set-key (kbd "<f8>")	   'neotree-toggle)
+(global-set-key (kbd "C-c a")	   'align-regexp)
+(global-set-key (kbd "C-z")		   'repeat) ;;stop accidentally hitting this and minimizing
+(global-set-key (kbd "C-c f")	   'follow-delete-other-windows-and-split) ;; Enter follow mode quickly
+(global-set-key (kbd "C-c n")	   'user--make-temp-file) ;;Create a temporary file that's a bit more persistant than scratch
 
 
 ;; Finalization ----------------------
