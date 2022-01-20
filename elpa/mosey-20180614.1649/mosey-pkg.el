b864f557b0124703a6273dab9b93abc47a316d4e
Noah M <noahcodes@protonmail.com>
2022-01-20 09:24:19 -0600

    Stop tracking elpa folder

--- elpa/mosey-20180614.1649/mosey-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from mosey.el  -*- no-byte-compile: t -*-
-(define-package "mosey" "20180614.1649" "Mosey around your buffers" '((emacs "24.4")) :commit "2e3ac9d334fa2937ed5267193dfd25d8e1f14dc2" :authors '(("Adam Porter" . "adam@alphapapa.net")) :maintainer '("Adam Porter" . "adam@alphapapa.net") :keywords '("convenience") :url "http://github.com/alphapapa/mosey.el")
